f0bf93b423689db68810a5430e56051aaf2f7c5f
hayatulemon <hayatulemon340@gmail.com>
2021-11-11 19:44:29 +0600

    added a new product a product page

--- src/Pages/Dashboard/AddNewProduct/AddNewProduct.js
+++ src/Pages/Dashboard/AddNewProduct/AddNewProduct.js
@@ -1,9 +1,32 @@
+import axios from 'axios';
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { useForm } from "react-hook-form";
 
 const AddNewProduct = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        axios.post('http://localhost:5050/addProduct', data)
+            .then(result => {
+                if (result.data.insertedId) {
+                    alert('Added a new product!')
+                }
+            })
+    }
     return (
-        <div>
-            <h2>Add a new Product</h2>
+        <div className="py-5">
+            <h2 className="pb-5 text-center">Add a New Product</h2>
+            <Container>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input className="form-control" {...register("name", { required: true })} placeholder="Product Name" /> <br />
+                    <input className="form-control" {...register("img", { required: true })} placeholder="Image Url" /> <br />
+                    <textarea className="form-control" {...register("des", { required: true })} placeholder="Description" /> <br />
+                    <input className="form-control" type="number" {...register("price", { required: true })} placeholder="Price" /> <br />
+                    <div className="text-center">
+                        <input className="btn btn-primary btn-lg" type="submit" value="Add a New Product" />
+                    </div>
+                </form>
+            </Container>
         </div>
     );
 };
